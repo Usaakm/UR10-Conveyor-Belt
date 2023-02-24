292cca81231ff63c5a555955f685c2d86705103e
Usama Akmal Muhammad <usaakm1999@gmail.com>
2023-02-24 21:34:56 +0000

    Initial commit

---
+++ Pick&Place.py
@@ -0,0 +1,80 @@
+import urx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Create a robot object
+# robot = urx.Robot("192.168.1.96")
+
+# # Move the robot to a joint position
+# joint_position = [0, -1.57, 1.57, -1.57, -1.57, 0]
+# robot.movej(joint_position)
+
+# # Move the robot to a Cartesian position
+# cartesian_position = [0.3, 0.3, 0.3, 0, 0, 0]
+# robot.movel(cartesian_position)
+
+# # Read the robot's current joint positions
+# joint_positions = robot.getj()
+# print(joint_positions)
+
+# # Close the connection to the robot
+# robot.close()
